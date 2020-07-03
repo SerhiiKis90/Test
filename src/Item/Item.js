6d97e6a36536547ae9eb17273b01f5dd61b72ff3
Serhii Kis <4irius@gmail.com>
2020-07-03 20:24:01 +0300

    Adding some basic structure

---
+++ src/Item/Item.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+import React from 'react';
+
+
+class Item extends Component {
+    render(){
+        return(
+            <h1>asd</h1>
+        )
+    }
+}
+
+
+export default Item;
